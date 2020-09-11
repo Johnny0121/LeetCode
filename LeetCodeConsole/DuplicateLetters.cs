7ca266de6a8988cf55e30824c87b7abec35c3954
Johnny Le <contact.lejohnny@gmail.com>
2020-09-11 23:21:55 +0100

    Added very simple problems

---
+++ LeetCodeConsole/DuplicateLetters.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace LeetCodeConsole
+{
+    // Takes in a string of letters, returns letters that are duplicated
+
+    public static class DuplicateLetters
+    {
+        public static void Run()
+        {
+            CheckForDuplicateLetters("indivisibilities");
+        }
+
+        public static void CheckForDuplicateLetters(string input)
+        {
+            IDictionary<char, int> _Letters = new Dictionary<char, int>();
+
+            foreach (char letter in input)
+            {
+                if (!_Letters.ContainsKey(letter))
+                {
+                    _Letters.Add(new KeyValuePair<char, int>(letter, 1));
+                }
+                else
+                {
+                    _Letters[letter] += 1;
+                }
+            }
+
+            //string output = string.Join(", ", _Letters.Where(x => x.Value > 1).Select(x => $"{x.Key} ({x.Value})"));
+            Console.WriteLine(_Letters.Where(x => x.Value > 1).Count());
+        }
+    }
+}
---
+++ LeetCodeConsole/HighestScoringWord.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace LeetCodeConsole
+{
+    public static class HighestScoringWord
+    {
+        public static void Run()
+        {
+            HighestScoring("what time are we climbing up to the volcano");
+        }
+
+        public static void HighestScoring(string sentence)
+        {
+            string[] _Sentence = sentence.Split(" ");
+
+            int _HighestScore = 0;
+            string _HighestScoreWord = string.Empty;
+
+            foreach (string word in _Sentence)
+            {
+                if (GetScore(word) > _HighestScore)
+                {
+                    _HighestScore = GetScore(word);
+                    _HighestScoreWord = word;
+                }
+            }
+
+            Console.WriteLine($"{_HighestScoreWord} - {_HighestScore} points");
+        }
+
+        public static int GetScore(string word)
+        {
+            int _Score = 0;
+
+            foreach (char letter in word)
+            {
+                _Score += char.ToLower(letter) - 96;
+            }
+
+            return _Score;
+        }
+    }
+}
