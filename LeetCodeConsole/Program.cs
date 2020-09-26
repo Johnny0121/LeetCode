ca7d0dd08cb39e084545e09f4f8bcc23558fcccc
Johnny Le <contact.lejohnny@gmail.com>
2020-09-26 01:17:47 +0100

    Added validate sub-sequence problem

--- LeetCodeConsole/Program.cs
+++ LeetCodeConsole/Program.cs
@@ -4,7 +4,7 @@
     {
         static void Main(string[] args)
         {
-            SimpleTwoSum.Run();
+            ValidateSubSequence.Run();
         }
     }
 }
---
+++ LeetCodeConsole/ValidateSubSequence.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Linq;
+
+namespace LeetCodeConsole
+{
+    // Problem: https://gyazo.com/4ee4c4c2a3c7edd921919abacbd32be8
+
+    public static class ValidateSubSequence
+    {
+        public static void Run()
+        {
+            int[] _Array = new int[] { 5, 1, 22, 25, 6, -1, 8, 10 };
+            int[] _Sequence = new int[] { 1, 6, -1, 10 };
+
+            Console.WriteLine($"Valid Subsequence: {IsValidSubsequence(_Array, _Sequence)}");
+        }
+
+        public static bool IsValidSubsequence(int[] array, int[] sequence)
+        {
+            int _Pointer = 0;
+            while (_Pointer < array.Length)
+            {
+                if (array[_Pointer] == sequence[0])
+                {
+                    sequence = sequence.Skip(1).ToArray();
+                }
+                _Pointer++;
+            }
+
+            return sequence.Length > 0 ? false : true;
+        }
+    }
+}
